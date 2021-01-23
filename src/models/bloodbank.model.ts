ec0fe06e738f38c0df70a68dc9be7db23c4a9f54
shraddhasale <shraddha.sale2000@gmail.com>
2021-01-23 15:55:44 +0530

    bloodbank model repository controller

---
+++ src/models/bloodbank.model.ts
@@ -0,0 +1,123 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Bloodbank extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  contactno: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  firstname: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  address: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  country: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  state: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  city: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  pincode: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  landmark: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  location: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  emailid: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  thumbnail: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  workingdays: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  day: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  statusId: number;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  createdAt: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  updatedAt: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Bloodbank>) {
+    super(data);
+  }
+}
+
+export interface BloodbankRelations {
+  // describe navigational properties here
+}
+
+export type BloodbankWithRelations = Bloodbank & BloodbankRelations;
